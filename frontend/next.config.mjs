f1d89ab573b9c555d8c93f905cb99556b78aa04c
Harman-Sahota <10harmansahota@gmail.com>
2024-05-02 13:39:53 -0400

    project setup

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-const withTM = require('next-transpile-modules')(['tailwindcss']);
-
-module.exports = withTM();
-
 export default nextConfig;
