fda0d09a6ec6242ac3f6e6ff2abdb03eeeedb0f8
Harman-Sahota <10harmansahota@gmail.com>
2024-05-04 13:41:43 -0400

    adding menu bar with mode toggle

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 
 import { cn } from "@/lib/utils"
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -24,7 +25,19 @@ export default function RootLayout({
       <body className={cn(
         "min-h-screen bg-background font-sans antialiased",
         fontSans.variable
-      )}>{children}</body>
-    </html>
+      )}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+
+          {children}
+
+        </ThemeProvider>
+
+      </body>
+    </html >
   );
 }
---
+++ frontend/components/ui/navbar.tsx
@@ -0,0 +1,34 @@
+import {
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarTrigger,
+} from "@/components/ui/menubar";
+import { ChevronDown } from "lucide-react";
+import { ModeToggle } from "./darkmode";
+
+export default function Navbar() {
+    return (
+        <div className="flex justify-between items-center w-full mt-6 px-6">
+            <h1 className="text-lg font-bold">Journey Maps</h1>
+            <Menubar>
+                <MenubarMenu>
+                    <MenubarTrigger>Home</MenubarTrigger>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger>Product <ChevronDown className="w-4 h-4 inline ml-1" /> </MenubarTrigger>
+                    <MenubarContent>
+                        <MenubarItem>Values</MenubarItem>
+                        <MenubarItem>How It Works</MenubarItem>
+                        <MenubarItem>Documentation</MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger>Contact Us</MenubarTrigger>
+                </MenubarMenu>
+            </Menubar>
+            <ModeToggle />
+        </div>
+    );
+}
