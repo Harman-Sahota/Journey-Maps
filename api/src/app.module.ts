7d4241c201d408022b1af6a4158ce183e4999998
Harman-Sahota <10harmansahota@gmail.com>
2024-05-15 18:15:45 -0400

    removing mongodb as i'll be using prisma , neondb with postgresql

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,21 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+
 
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-    MongooseModule.forRoot(process.env.MONGODB_CONNECT_URL),
-    UserModule
-  ],
-  controllers: [],
-  providers: [],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
