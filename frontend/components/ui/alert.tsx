e998e21437f10453e8ed31920efdc46982732258
Harman-Sahota <10harmansahota@gmail.com>
2024-05-19 19:04:45 -0400

    trial

--- frontend/components/ui/alert.tsx
+++ frontend/components/ui/alert.tsx
@@ -3,7 +3,7 @@
     export default function Alert() {
 
         return (
-            <a href='http://localhost:8000/api/auth/discord'>
+            <a href='https://journeymaps.life/api/auth/discord'>
                 <Button className="mt-4 rounded dark:text-white">Get Started</Button>
             </a>
         );
